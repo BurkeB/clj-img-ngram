f4fe020e16306a681c25e0c8dfdd8828e5218c55
Bruno Burke <brunoburke@googlemail.com>
2016-05-31 00:07:28 +0200

    still not working

--- src/clj_img_ngram/core.clj
+++ src/clj_img_ngram/core.clj
@@ -2,6 +2,7 @@
   (:use [mikera.image.core])
   (:require [clojure.java.io :as io]
             [mikera.image.filters :as filt]
+            [mikera.image.colours :as c]
             [clj-img-ngram.tree :as tree]
             [clj-img-ngram.ngram :as ngram]))
 
@@ -13,14 +14,34 @@
 ;; show the iamge, after applying an "invert" filter
 ;(show (filter-image ant (filt/invert)))
 
-;(show ant)
+(show ant)
 
 (def pixels (get-pixels ant))
 
 ;(count (:down (ngram/collect-ngrams ant 4)))
-;(def tree (tree/generate (:down (ngram/collect-ngrams ant 4)) (tree/make-node "root")))
+;(def tree (tree/generate (take 100000 (:down (ngram/collect-ngrams ant 4))) (tree/make-node "root")))
+
+;(ngram/probability tree (take 2 (first (:down (ngram/collect-ngrams ant 4)))))
+;(ngram/probability tree (second (:down (ngram/collect-ngrams ant 4))))
 
 
+;(count pixels)
+;(ngram/next-possibilities tree (take 1 (first (:down (ngram/collect-ngrams ant 4)))))
+;(ngram/next-possibilities tree '())
+
+(defn change-pixels [pixels i]
+  (let [seq (map c/components-rgb (list (get pixels (- i 1)) (get pixels (- i 2))))
+        next (first (ngram/next-possibilities tree seq))]
+    (aset pixels i (apply c/rgb next))
+    (if (>= i (dec (count pixels)))
+      pixels
+      (recur pixels (inc i)))))
+;; wrong color values in "next"...
+
+;(change-pixels pixels 100000)
+;(set-pixels ant pixels)
+;(show ant)
+;(ngram/next-possibilities tree)
 ;(get-child (get-ngram-tree (take 8 (:right (collect-ngrams ant 4))) (make-node "root")) -11320525)
 
 (defn -main
--- src/clj_img_ngram/ngram.clj
+++ src/clj_img_ngram/ngram.clj
@@ -2,20 +2,21 @@
   (:use [mikera.image.core])
   (:require [clojure.java.io :as io]
             [mikera.image.filters :as filt]
+            [mikera.image.colours :as c]
             [clj-img-ngram.tree :as tree])
   (:gen-class))
 
 (defn collect-down [pixels i n img-width img-height set]
   (if (or (= 0 n) (>= (Math/floor (/ i img-width)) img-height))
     set
-    (recur pixels (+ i img-width) (dec n) img-width img-height (conj set (get pixels i)))))
+    (recur pixels (+ i img-width) (dec n) img-width img-height (conj set (c/components-rgb (get pixels i))))))
 
 (defn collect-right [pixels i n img-width img-height set]
   (if (= 0 n)
     set
     (if (>= (inc (mod i img-width)) img-width)
       (conj set (get pixels i))
-      (recur pixels (inc i) (dec n) img-width img-height (conj set (get pixels i))))))
+      (recur pixels (inc i) (dec n) img-width img-height (conj set (c/components-rgb (get pixels i)))))))
 
 (defn collect-ngrams [img n]
   (let [pixels (get-pixels img)
@@ -34,7 +35,24 @@
       0
       (recur (second child) (rest ngram))))))
 
-(defn next-possibilities [tree]
-  (map :id (tree/get-children tree)))
+(defn probability [tree ngram]
+  (let [p1 (count-ngram tree ngram)
+        p2 (count-ngram tree (drop-last 1 ngram))]
+    (println p1)
+    (println p2)
+    (if (or (= p1 0) (= p2 0))
+      0
+      (/ p1 p2))))
 
-(defn probability [])
+(drop-last 1 '(1 2 3 4 5))
+
+(defn next-possibilities
+  ([tree]
+    (map :id (tree/get-children tree)))
+  ([tree ngram]
+    (if (empty? ngram)
+      (map :id (tree/get-children tree))
+      (let [child (tree/get-child tree (first ngram))]
+        (if (empty? child)
+          (next-possibilities tree '())
+          (recur (second child) (rest ngram)))))))
